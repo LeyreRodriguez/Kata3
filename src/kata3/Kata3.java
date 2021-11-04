23a10e6ab8b876f56c7fd361e7d34e341a5e0299
Leyre <Leyre@DESKTOP-NO4GJ0P>
2021-11-04 17:21:28 +0000

    Kata3 version1

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,17 +1,10 @@
 package kata3;
+
 public class Kata3 {
     public static void main(String[] args) {
-        Histogram<String> histo = new Histogram<>();
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("ulpgc.com");
-        histo.increment("ulpgc.com");
-        histo.increment("ulpgc.com");
-        histo.increment("ulpgc.com");  
-        histo.increment("ull.com");  
-        histo.increment("ull.com");
-        histo.increment("ull.com");
-        histo.increment("hotmail.com");   
-        new HistogramDisplay(histo).execute();
+        // TODO code application logic here
+        
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
     }
 }
